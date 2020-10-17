30eca58b5fb6da7bc4dae2a0cac751813a90c9a8
Vinodh Kumar Konda <53635507+vinodhkumar-k@users.noreply.github.com>
2020-10-17 13:45:29 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,19 @@
 const express = require('express'),
       router = express.Router(),
-      expensesController = require('../controllers/expensesController');
+      expensesController = require('../controllers/expensesController'),
+      Expense = require('../models/expenses');
 
 router.get('/expenses', expensesController.getExpenses);
+
+router.post('/expenses', (req, res) => {
+  let expense = new Expense(req.body);
+  expense.save((err, exp) => {
+    console.log('saving')
+    if (err) {
+      return res.status(400).json(err);
+    }
+    return res.status(200).json(exp);
+  });
+});
 
 module.exports = router;
