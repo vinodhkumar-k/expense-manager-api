1b0054836b70068a12e826774bf83a3ef52fc476
Vinodh Kumar Konda <53635507+vinodhkumar-k@users.noreply.github.com>
2020-10-17 13:59:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,8 +9,24 @@ const port = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
 app.use('/api', router);
 
-app.listen(port, () => {
-  console.log(`Running on port ${port}`);
+// Started DB Code -- Need to move to file in models called db.js
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://127.0.0.1:27017/ExpenseManager", { useNewUrlParser: true }); // the url should come from config file
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+  // below code should be independent 
+  app.listen(port, () => {
+    console.log(`Running on port ${port}`);
+  });
 });
+// Ended DB Code
+// app.listen(port, () => {
+//   console.log(`Running on port ${port}`);
+// });
