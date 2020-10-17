15d98928b6ebdabcd782e28abd3018368b1344fc
Vinodh Kumar Konda <53635507+vinodhkumar-k@users.noreply.github.com>
2020-10-17 09:48:34 +0530

    Update expenses.js

---
+++ models/expenses.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const expensesSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  userName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  expenses: [expenseDetailsSchema]
+});
