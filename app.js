1e223f43633335009ced07fecf1a13f66a93b322
Vinodh Kumar Konda <53635507+vinodhkumar-k@users.noreply.github.com>
2020-10-17 13:58:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express'); 
 const router = require('./routes/index');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
