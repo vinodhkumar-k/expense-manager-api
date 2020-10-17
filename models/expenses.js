b9f50daa4bc401c8056d9df0cd75cceeba79ad3f
Vinodh Kumar Konda <53635507+vinodhkumar-k@users.noreply.github.com>
2020-10-17 09:49:08 +0530

    Update expenses.js

--- models/expenses.js
+++ models/expenses.js
@@ -1,5 +1,23 @@
 const mongoose = require('mongoose');
 
+const expenseDetailsSchema = new mongoose.Schema({
+  category: {
+    type: String,
+    required: true
+  },
+  amount: {
+    type: Number,
+    required: true
+  },
+  details: {
+    type: String
+  },
+  date: {
+    type: Date,
+    required: true
+  }
+});
+
 const expensesSchema = new mongoose.Schema({
   userId: {
     type: String,
