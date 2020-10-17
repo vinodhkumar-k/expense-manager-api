2a15cc32176bffb3adaeb9ed10a05d07e86e68d4
Vinodh Kumar Konda <53635507+vinodhkumar-k@users.noreply.github.com>
2020-10-17 13:59:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const app = express();
 
 const port = process.env.PORT || 3001;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/api', router);
 
 app.listen(port, () => {
