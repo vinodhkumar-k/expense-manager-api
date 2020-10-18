8e2cf106ecc3d618bbb9b5b30427b3c215f120d6
Vinodh Kumar Konda <53635507+vinodhkumar-k@users.noreply.github.com>
2020-10-18 09:55:54 +0530

    Update expensesController.js

--- controllers/expensesController.js
+++ controllers/expensesController.js
@@ -1,3 +1,5 @@
+const Expense = require('../models/expenses');
+
 const getExpenses = (req, res) => {
   res.json({expenses: {
     date: '16/10/2020',
@@ -7,4 +9,16 @@ const getExpenses = (req, res) => {
   }});
 };
 
+const addExpense = (req, res) => {
+  let expense = new Expense(req.body);
+  expense.save((err, exp) => {
+    console.log('saving')
+    if (err) {
+      return res.status(400).json(err);
+    }
+    return res.status(200).json(exp);
+  });
+};
+
 exports.getExpenses = getExpenses;
+exports.addExpense = addExpense;
