3285c31d3371630e41ed0027c7785eb4e9c47fa5
Vinodh Kumar Konda <53635507+vinodhkumar-k@users.noreply.github.com>
2020-10-23 11:04:27 +0530

    Update expensesService.js

--- services/expensesService.js
+++ services/expensesService.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose'),
 const Expense = require('../models/expenses');
 
 const addExpense = (expenseDetails) => {
-  /*expense.save((err, exp) => {
+  /*let expense = new Expense(expenseDetails);
+  expense.save((err, exp) => {
     console.log('saving')
     if (err) {
       return res.status(400).json(err);
@@ -13,22 +14,57 @@ const addExpense = (expenseDetails) => {
   const deferred = q.defer();
   const userId = expenseDetails.userId;
   const newExpense = {
-    expenseId: expenseDetails.expenses[0].expenseId,
-    date: expenseDetails.expenses[0].date,
-    category: expenseDetails.expenses[0].category,
-    amount: expenseDetails.expenses[0].amount,
-    details: expenseDetails.expenses[0].details
+    expenseId: expenseDetails.expenses.expenseId,
+    month: expenseDetails.expenses.month,
+    date: expenseDetails.expenses.date,
+    category: expenseDetails.expenses.category,
+    amount: expenseDetails.expenses.amount,
+    details: expenseDetails.expenses.details
   };
-  console.log(expenseDetails);
-  Expense.findById(userId).exec((err, user) => {
+  Expense.find({userId: userId}).exec((err, user) => {
+    console.log(user);
+    console.log('---------------');
     if (err) {
       deferred.reject({"status": 500, "jsonResult": {"result": err}});
-    } else if (!user) {
-      deferred.reject({"status": 404, "jsonResult": {"result": "User not found"}});
+    } else if (!user.length) {
+      console.log('aa');
+      //deferred.reject({"status": 404, "jsonResult": {"result": "User not found"}});
+      new Expense(expenseDetails).save((error, result) => {
+        if (error) {
+          console.log("Error occured is");
+          console.log(error);
+          return;
+        }
+        console.log("Result is: ");
+        console.log(result);
+        return;
+      })
     } else {
-      console.log("User found implement business logic here")
+      Expense.update(
+        {userId: userId},
+        {$push: {expenses: {month: expenseDetails.expenses.month, expenditure: [newExpense]}}},
+        (error, result) => {
+          console.log('bb');
+          if (error) {
+            deferred.reject({"status": 500, "jsonResult": {"result": error}})
+          } else if (!result) {
+            deferred.reject({"status": 404, "jsonResult": {"result": "Could not find the expenses"}});
+          } else {
+            console.log("Successfully added expense");
+            console.log(mongoose.Types.ObjectId(userId));
+            console.log(result);
+            console.log(user);
+            deferred.resolve({"status": 200, "jsonResult": {"result": result}});
+          }
+        }
+      );
     }
   });
+  /*Expense.find({userId}).exec((err, user) => {
+    console.log(err);
+    console.log(user[0].expenses);
+  })*/
+  return deferred.promise;
 };
 
 exports.addExpense = addExpense;
