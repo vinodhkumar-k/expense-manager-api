6dd88b99e11c0048ffa250341de9662a5c06b157
Vinodh Kumar Konda <53635507+vinodhkumar-k@users.noreply.github.com>
2020-10-23 13:08:08 +0530

    Update expensesService.js

--- services/expensesService.js
+++ services/expensesService.js
@@ -1,33 +1,16 @@
 const mongoose = require('mongoose'),
-      q = require('q');
+  q = require('q'),
+  _ = require('lodash');
 const Expense = require('../models/expenses');
 
 const addExpense = (expenseDetails) => {
-  /*let expense = new Expense(expenseDetails);
-  expense.save((err, exp) => {
-    console.log('saving')
-    if (err) {
-      return res.status(400).json(err);
-    }
-    return res.status(200).json(exp);
-  });*/
   const deferred = q.defer();
   const userId = expenseDetails.userId;
-  const newExpense = {
-    expenseId: expenseDetails.expenses.expenseId,
-    month: expenseDetails.expenses.month,
-    date: expenseDetails.expenses.date,
-    category: expenseDetails.expenses.category,
-    amount: expenseDetails.expenses.amount,
-    details: expenseDetails.expenses.details
-  };
-  Expense.find({userId: userId}).exec((err, user) => {
-    console.log(user);
-    console.log('---------------');
+  const newExpense = {...expenseDetails.expenses.expenditure[0]};
+  Expense.find({ userId: userId }).exec((err, user) => {
     if (err) {
-      deferred.reject({"status": 500, "jsonResult": {"result": err}});
+      deferred.reject({ "status": 500, "jsonResult": { "result": err } });
     } else if (!user.length) {
-      console.log('aa');
       //deferred.reject({"status": 404, "jsonResult": {"result": "User not found"}});
       new Expense(expenseDetails).save((error, result) => {
         if (error) {
@@ -40,30 +23,39 @@ const addExpense = (expenseDetails) => {
         return;
       })
     } else {
-      Expense.update(
-        {userId: userId},
-        {$push: {expenses: {month: expenseDetails.expenses.month, expenditure: [newExpense]}}},
-        (error, result) => {
-          console.log('bb');
-          if (error) {
-            deferred.reject({"status": 500, "jsonResult": {"result": error}})
-          } else if (!result) {
-            deferred.reject({"status": 404, "jsonResult": {"result": "Could not find the expenses"}});
-          } else {
-            console.log("Successfully added expense");
-            console.log(mongoose.Types.ObjectId(userId));
-            console.log(result);
-            console.log(user);
-            deferred.resolve({"status": 200, "jsonResult": {"result": result}});
+      let expensesForMonth = _.find(user[0].expenses, { month: expenseDetails.expenses.month });
+      if (expensesForMonth) {
+        Expense.update(
+          { userId: userId,  "expenses.month": expenseDetails.expenses.month},
+          // { $push: { expenses: { month: expenseDetails.expenses.month, expenditure: [newExpense] } } },
+          {$push: {"expenses.$.expenditure": newExpense}},
+          (error, result) => {
+            if (error) {
+              deferred.reject({ "status": 500, "jsonResult": { "result": error } })
+            } else if (!result) {
+              deferred.reject({ "status": 404, "jsonResult": { "result": "Could not find the expenses" } });
+            } else {
+              deferred.resolve({ "status": 200, "jsonResult": { "result": result } });
+            }
           }
-        }
-      );
+        );
+      } else {
+        Expense.update(
+          { userId },
+          {$addToSet: {expenses: {month: expenseDetails.expenses.month, expenditure: [newExpense]}}},
+          (error, result) => {
+            if (error) {
+              deferred.reject({ "status": 500, "jsonResult": { "result": error } });
+            } else if (!result) {
+              deferred.reject({ "status": 404, "jsonResult": { "result": "Could not find the expenses" } });
+            } else {
+              deferred.resolve({ "status": 200, "jsonResult": { "result": result } });
+            }
+          }
+        );
+      }
     }
   });
-  /*Expense.find({userId}).exec((err, user) => {
-    console.log(err);
-    console.log(user[0].expenses);
-  })*/
   return deferred.promise;
 };
 
