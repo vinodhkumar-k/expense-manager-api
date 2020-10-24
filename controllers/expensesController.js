dbbdfdd86fe1f8de37654f3e30a0bcee8252e949
Vinodh Kumar Konda <53635507+vinodhkumar-k@users.noreply.github.com>
2020-10-24 21:26:02 +0530

    Update expensesController.js

--- controllers/expensesController.js
+++ controllers/expensesController.js
@@ -8,15 +8,21 @@ const sendJSONResponse = (res, responseObj) => {
 const getExpensesByUser = (req, res) => {
   expensesService.getExpensesByUser(req.params.id)
     .then(responseObj => sendJSONResponse(res, responseObj))
-    .catch(responseObj => sendJSONResponse(res, responseObj))
+    .catch(responseObj => sendJSONResponse(res, responseObj));
 };
 
+const getUserExpensesByMonth = (req, res) => {
+  expensesService.getUserExpensesByMonth(req.params.id, req.params.month)
+  .then(responseObj => sendJSONResponse(res, responseObj))
+  .catch(responseObj => sendJSONResponse(res, responseObj));
+};
 
 const addExpense = (req, res) => {
   expensesService.addExpense(req.body)
     .then(responseObj => sendJSONResponse(res, responseObj))
-    .catch(responseObj => sendJSONResponse(res, responseObj))
+    .catch(responseObj => sendJSONResponse(res, responseObj));
 };
 
 exports.getExpensesByUser = getExpensesByUser;
 exports.addExpense = addExpense;
+exports.getUserExpensesByMonth = getUserExpensesByMonth;
