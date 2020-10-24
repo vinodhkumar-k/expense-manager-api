749cf5ab7e5bdc2d9496e4a1b5b63f5848b84515
Vinodh Kumar Konda <53635507+vinodhkumar-k@users.noreply.github.com>
2020-10-25 00:11:31 +0530

    Update expensesController.js

--- controllers/expensesController.js
+++ controllers/expensesController.js
@@ -29,7 +29,19 @@ const updateExpense = (req, res) => {
     .catch(responseObj => sendJSONResponse(res, responseObj));
 };
 
+const deleteExpense = (req, res) => {
+  const expenseDetails = {
+    userId: req.params.id,
+    month: req.params.month,
+    expenseId: Number(req.params.expenseId)
+  }
+  expensesService.deleteExpense(expenseDetails)
+    .then(responseObj => sendJSONResponse(res, responseObj))
+    .catch(responseObj => sendJSONResponse(res, responseObj));
+};
+
 exports.getExpensesByUser = getExpensesByUser;
 exports.addExpense = addExpense;
 exports.getUserExpensesByMonth = getUserExpensesByMonth;
 exports.updateExpense = updateExpense;
+exports.deleteExpense = deleteExpense;
