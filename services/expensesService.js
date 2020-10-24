f77312ba891909d1718ee11033dce7f89b863596
Vinodh Kumar Konda <53635507+vinodhkumar-k@users.noreply.github.com>
2020-10-24 23:08:53 +0530

    Update expensesService.js

--- services/expensesService.js
+++ services/expensesService.js
@@ -21,10 +21,11 @@ const getUserExpensesByMonth = (userId, month) => {
     if (err) {
       deferred.reject({ "status": 500, "jsonResult": { "result": error } });
     } else {
-      if (!value) {
+      const expensesForMonth = _.find(result[0].expenses, {month});
+      if (!expensesForMonth) {
         deferred.reject({ "status": 500, "jsonResult": { "result": "No expenses for given month" } });
       }
-      deferred.resolve({ "status": 200, "jsonResult": { "result": _.find(result[0].expenses, {month}) } });
+      deferred.resolve({ "status": 200, "jsonResult": { "result":  expensesForMonth} });
     }
   });
   return deferred.promise;
@@ -38,7 +39,6 @@ const addExpense = (expenseDetails) => {
     if (err) {
       deferred.reject({ "status": 500, "jsonResult": { "result": err } });
     } else if (!user.length) {
-      //deferred.reject({"status": 404, "jsonResult": {"result": "User not found"}});
       new Expense(expenseDetails).save((error, result) => {
         if (error) {
           deferred.reject({ "status": 500, "jsonResult": { "result": error } });
@@ -51,7 +51,6 @@ const addExpense = (expenseDetails) => {
       if (expensesForMonth) {
         Expense.update(
           { userId: userId,  "expenses.month": expenseDetails.expenses.month},
-          // { $push: { expenses: { month: expenseDetails.expenses.month, expenditure: [newExpense] } } },
           {$push: {"expenses.$.expenditure": newExpense}},
           (error, result) => {
             if (error) {
@@ -97,15 +96,8 @@ const updateExpense = (expenseDetails) => {
       if (!expense) {
         deferred.reject({"status": 404, "jsonResult": {"result": "Could not find the expenses"}});
       } else {
-        const expenditure = _.find(expense.expenditure, {expenseId: expenseDetails.expenses.expenditure[0].expenseId});
         const expenditureIndex = _.findIndex(expense.expenditure, {expenseId: expenseDetails.expenses.expenditure[0].expenseId});
-        const updateExpenditure = expenseDetails.expenses.expenditure[0];
-        expenditure.date = updateExpenditure.date;
-        expenditure.category = updateExpenditure.category;
-        expenditure.amount = updateExpenditure.amount;
-        expenditure.details = updateExpenditure.details;
-
-        user[0].expenses[expenseIndex]['expenditure'][expenditureIndex] = expenditure;
+        user[0].expenses[expenseIndex]['expenditure'][expenditureIndex] = {...expenseDetails.expenses.expenditure[0]};
         user[0].markModified("expenses");
         user[0].save((error, updatedUser) => {
           if (error) {
