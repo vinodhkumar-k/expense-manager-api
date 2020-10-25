ab85fb148c1988995dd968347ce578e40d5698c4
Vinodh Kumar Konda <53635507+vinodhkumar-k@users.noreply.github.com>
2020-10-25 22:32:07 +0530

    Update analyticsService.js

--- services/analyticsService.js
+++ services/analyticsService.js
@@ -18,4 +18,22 @@ const getTotalExpensesForAllMonths = (userId) => {
   return deferred.promise;
 };
 
+const getCategoryWiseExpensesForAMonth = (userId, month) => {
+  const deferred = q.defer();
+  Expense.aggregate([
+    {$match: {userId}},
+    {$unwind: "$expenses"},
+    {$match: {"expenses.month": month}},
+    {$unwind: "$expenses.expenditure"},
+    {$group: {_id: "$expenses.expenditure.category", total: {$sum: "$expenses.expenditure.amount"}}}
+  ], (err, res) => {
+    if (err) {
+      deferred.reject({ "status": 500, "jsonResult": { "result": err } });
+    }
+    deferred.resolve({ "status": 200, "jsonResult": { "result": res } });
+  });
+  return deferred.promise;
+};
+
 exports.getTotalExpensesForAllMonths = getTotalExpensesForAllMonths;
+exports.getCategoryWiseExpensesForAMonth = getCategoryWiseExpensesForAMonth;
